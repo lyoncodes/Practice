7c32c0e929e7c60ae17c3d0d62b626839ca9df94
Michael Lyon <lyoncodes@gmail.com>
2018-11-05 19:56:48 -0800

    installs canvas heading

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 'use strict'
 console.log('mainJs is loaded')
+// paper js
 var paper
-paper.install(window) // Installs paper as global variable
-paper.setup(document.getElementById('mainCanvas')) // attaches paper to canvas
+paper.install(window)
+paper.setup(document.getElementById('mainCanvas'))
+
+var mainCanvas = document.getElementById('mainCanvas')
+mainCanvas.style.margin = '100px 250px'
+
 // TODO
+var clickTool = new Tool()
+
+var c = Shape.Circle(160, 160, 80)
+c.fillColor = 'black'
+
+var text = new PointText(160, 160)
+text.justification = 'center'
+text.fillColor = 'white'
+text.fontSize = 20;
+text.content = 'Pirate Game'
+
+clickTool.onMouseDown = (e) => {
+  var c = Shape.Circle(e.point.x, e.point.y, 20)
+  c.fillColor = 'black'
+}
+
 paper.view.draw()
