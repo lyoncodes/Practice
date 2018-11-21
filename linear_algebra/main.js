77985bfa7317df824a72ae14a826e2fddfbbc7d5
Michael Lyon <lyoncodes@gmail.com>
2018-11-21 08:43:46 -0800

    adds style to homepage

--- linear_algebra/main.js
+++ linear_algebra/main.js
@@ -2,19 +2,20 @@
 $(document).ready(function () {
   // jQuery goes here
 })
-
+// RIGHT
 $(function () {
   var darkMatter = $('<div></div>').height(100)
-  $('#left').css({float: 'left', width: '50%', height: '100%'})
+  $('#left').css({float: 'left', width: '50%', height: '100%', 'background-color': '#d9ffcc'})
   $('#mainTitle').html('Michael Lyon')
-  $('#mainTitle').css({marginLeft: '125px', float: 'left'})
+  $('#mainTitle').css({'text-align': 'center'})
   $('#mainTitle').before(darkMatter)
 })
+// LEFT
 $(function () {
   var darkMatter = $('<div></div>').height(200)
-  $('#right').css({float: 'right', width: '50%'})
+  $('#right').css({float: 'left', width: '50%', height: '100%', 'background-color': '#ffffcc'})
   $('.links').before(darkMatter)
-  $('.links a').css({margin: 'auto 250px', display: 'block'})
+  $('.links a').css({float: 'left', width: '33%', margin: '12px 33.33%', padding: '15px', border: '1px solid #d9ffcc', 'text-align': 'center'})
 })
 
 // factorial function
@@ -27,3 +28,16 @@ function factorial (number) {
   }
 }
 console.log(factorial(20))
+// two dimensional array
+Array.matrix = function (numrows, numcols, initializer) {
+  var arr = []
+  for (var i = 0; i < numrows; i++) {
+    var columns = []
+    for (var j = 0; j < numcols; j++) {
+      columns[j] = initializer
+    }
+    arr[i] = columns
+  }
+  return arr
+}
+
