0e04ee2df787e9e2e67ae76d8e20fc78aefcb867
Michael Lyon <lyoncodes@gmail.com>
2019-03-28 17:38:18 -0700

    creates Player object and instantiates within searchPlayer function

--- api/server.js
+++ api/server.js
@@ -42,19 +42,31 @@ function getPlayer (req, res) {
 }
 
 function searchPlayer (searchStr) {
- const Player = NBA.findPlayer(searchStr)
- NBA.stats.playerInfo({ PlayerID: Player.playerId })
+ const player = NBA.findPlayer(searchStr)
+ NBA.stats.playerInfo({ PlayerID: player.playerId })
  .then (result => {
-  let college = result.commonPlayerInfo[0].school
-  console.log(college)
+  let newPlayer = new Player(result)
+  console.log(newPlayer)
+ })
+ NBA.stats.playerSplits({ PlayerID: player.playerId })
+ .then (result => {
+  console.log(result)
+  // let newPlayerStats = new PlayerStats(result)
+  // console.log(newPlayerStats)
  })
 }
 
-// getPlayer() formats query for searchPlayer()
-// create Player object for basic return properties
-// crete a searchPlayer() function that instantiates Player
-
 // Objects
+function Player (obj) {
+ this.id = obj.commonPlayerInfo[0].personId;
+ this.name = obj.commonPlayerInfo[0].displayFirstLast;
+ this.position = obj.commonPlayerInfo[0].position;
+ this.team = obj.commonPlayerInfo[0].teamName;
+ this.draftYear = obj.commonPlayerInfo[0].draftYear;
+ this.draftRound = obj.commonPlayerInfo[0].draftRound;
+ this.draftNumber = obj.commonPlayerInfo[0].draftNumber;
+ this.college = obj.commonPlayerInfo[0].school;
+}
 
 // Listen
 app.listen(PORT, () => {
